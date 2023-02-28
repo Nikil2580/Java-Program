ebeabd3a487c8ba8ddc32c01953eb50eea6fbc02
Nikil2580 <cyanikil002@gmail.com>
2023-03-01 00:03:40 +0530

    message: "File Edited"

--- Integer_Based_Problems/HCF_Or_GCD.java
+++ Integer_Based_Problems/HCF_Or_GCD.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 
-public class HCF_GCD {
+public class HCF_Or_GCD {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number1 = sc.nextInt();
         int number2 = sc.nextInt();
         sc.close();
-        while(number1!=number2){
-            if(number1 > number2){
+        while (number1 != number2) {
+            if (number1 > number2) {
                 number1 = number1 - number2;
-            }
-            else{
+            } else {
                 number2 = number2 - number1;
             }
         }
