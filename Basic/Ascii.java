70e49e7ccab5aba09149259b0e6d052e12abe482
Nikil2580 <cyanikil002@gmail.com>
2023-03-04 15:38:37 +0530

    Updated

--- Basic/Ascii.java
+++ Basic/Ascii.java
@@ -1,6 +1,7 @@
 public class Ascii {
     public static void main(String[] args) {
         char alpha = 'a';
-        System.out.println("ASCII value:" + (alpha-0));
+        System.out.println("ASCII value:" + (alpha - 0));
+        System.out.println("ASCII value:" + (int) alpha);
     }
 }
