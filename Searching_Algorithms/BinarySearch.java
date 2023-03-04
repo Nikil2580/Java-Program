969a06f02ef95cfb5125b9676e19204271b0a7f0
Nikil2580 <cyanikil002@gmail.com>
2023-03-04 15:31:53 +0530

    Add Searching Algorithms in Java

---
+++ Searching_Algorithms/BinarySearch.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[] { 23, 435, 1, 0, 6, 86, 279 };
+        int num = sc.nextInt();
+        Arrays.sort(arr);
+        int i = 0, j = arr.length - 1;
+        while (i <= j) {
+            int middle = i + (j - i) / 2; // To avoid array index out of bound we add i
+            if (num == arr[middle]) {
+                System.out.println("Found in");
+                return;
+            } else if (num > arr[middle]) {
+                i = middle + 1;
+            } else {
+                j = middle - 1;
+            }
+        }
+        System.out.println("Not Found");
+    }
+}
