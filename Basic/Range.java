940cea626cfed83bff5f85bd4abc82b3affc743c
Nikil2580 <cyanikil002@gmail.com>
2023-03-04 15:42:08 +0530

    Print range of int, char, float

---
+++ Basic/Range.java
@@ -0,0 +1,8 @@
+public class Range {
+    public static void main(String[] args) {
+
+        System.out.printf("Range of Integer: %d to %d\n", Integer.MIN_VALUE, Integer.MAX_VALUE);
+        System.out.printf("Range of Character: %c to %c\n", Character.MAX_VALUE, Character.MIN_VALUE);
+        System.out.println("Range of Float " + Float.MAX_VALUE + " to " + Float.MIN_VALUE);
+    }
+}
