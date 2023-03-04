baa14aa6b9b330254a233a1e3d2190249901a853
Nikil2580 <cyanikil002@gmail.com>
2023-03-04 15:37:04 +0530

    Add program that prints next 3rd Char

---
+++ Basic/AsciiAdd.java
@@ -0,0 +1,10 @@
+public class AsciiAdd {
+    public static void main(String[] args) {
+        char alpha1 = 'd';
+        int num = alpha1 + 3;
+        char alpha2 = (char) num;
+        System.out.println(alpha1);
+        System.out.println("After Conversion");
+        System.out.println(alpha2);
+    }
+}
