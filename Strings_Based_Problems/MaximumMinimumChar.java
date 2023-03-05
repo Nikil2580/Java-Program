19ad599a869b75b4eb6d5c45628b4f505142913e
Nikil2580 <cyanikil002@gmail.com>
2023-03-05 19:23:07 +0530

    Minimum Char is working fine

--- Strings_Based_Problems/MaximumMinimumChar.java
+++ Strings_Based_Problems/MaximumMinimumChar.java
@@ -1,19 +1,20 @@
 public class MaximumMinimumChar {
     public static void main(String[] args) {
-        String str = "malayalam";
+        String str = "aaaabbbbbccddd";
         int[] arr = new int[256];
         for (int i = 0; i < str.length(); i++) {
             arr[str.charAt(i)]++;
         }
-        int max = 0;
+        int max = Integer.MIN_VALUE;
         char maxCh = str.charAt(0);
-        int min = 1;
+        int min = Integer.MAX_VALUE;
         char minCh = str.charAt(0);
         for (int i = 0; i < 256; i++) {
             if (max < arr[i]) {
                 max = arr[i];
                 maxCh = (char) i;
-            } else if (min > arr[i]) {
+            }
+            if (min > arr[i] && arr[i] > 0) {
                 min = arr[i];
                 minCh = (char) i;
             }
