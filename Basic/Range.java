6b138e14495e9389792d6ab4685888390bac66b3
Nikil2580 <cyanikil002@gmail.com>
2023-03-10 21:49:50 +0530

    Updated

--- Basic/Range.java
+++ Basic/Range.java
@@ -1,8 +1,18 @@
 public class Range {
     public static void main(String[] args) {
 
-        System.out.printf("Range of Integer: %d to %d\n", Integer.MIN_VALUE, Integer.MAX_VALUE);
-        System.out.printf("Range of Character: %c to %c\n", Character.MAX_VALUE, Character.MIN_VALUE);
-        System.out.println("Range of Float " + Float.MAX_VALUE + " to " + Float.MIN_VALUE);
+        System.out.printf("Range of Integer:   %d to %d\n\n", Integer.MIN_VALUE, Integer.MAX_VALUE);
+
+        System.out.printf("Range of Short:     %d to %d\n\n", Short.MAX_VALUE, Short.MIN_VALUE);
+
+        System.out.printf("Range of Byte:      %d to %d\n\n", Byte.MAX_VALUE, Byte.MIN_VALUE);
+
+        System.out.printf("Range of Long:      %d to %d\n\n", Long.MIN_VALUE, Long.MAX_VALUE);
+
+        System.out.println("Range of Float:     " + Float.MAX_VALUE + " to " + Float.MIN_VALUE + "\n");
+
+        System.out.println("Range of Double:    " + Double.MAX_VALUE + " to " + Double.MIN_VALUE + "\n");
+
+        System.out.println("Range of Character: " + Character.MAX_VALUE + " to " + Character.MIN_VALUE);
     }
 }
