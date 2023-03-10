fcd89b18f60380351f7137c4aa2778b7044cac4e
Nikil2580 <cyanikil002@gmail.com>
2023-03-10 21:49:33 +0530

    Understand String[] args

---
+++ Basic/ReadingCmdLineArg.java
@@ -0,0 +1,7 @@
+public class ReadingCmdLineArg {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(args[1]);
+        System.out.println(args[0]);
+    }
+}
