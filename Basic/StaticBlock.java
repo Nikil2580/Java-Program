4575c633c6e828d181f09352675d48d6a715491d
Nikil2580 <cyanikil002@gmail.com>
2023-03-10 21:51:33 +0530

    Static block executes before main method

---
+++ Basic/StaticBlock.java
@@ -0,0 +1,9 @@
+public class StaticBlock {
+    public static void main(String[] args) {
+        System.out.println("CTS");
+    }
+
+    static {
+        System.out.println("Covai.Co");
+    }
+}
