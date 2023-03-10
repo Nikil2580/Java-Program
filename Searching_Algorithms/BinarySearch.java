a3574a57726f57e0f2a0ab3c26fece5fa0c0187b
NIKIL S <107380156+Nikil2580@users.noreply.github.com>
2023-03-10 22:22:01 +0530

    Update BinarySearch.java

--- Searching_Algorithms/BinarySearch.java
+++ Searching_Algorithms/BinarySearch.java
@@ -7,12 +7,14 @@ public class BinarySearch {
         int[] arr = new int[] { 23, 435, 1, 0, 6, 86, 279 };
         int num = sc.nextInt();
         sc.close();
+        
         Arrays.sort(arr);
         int i = 0, j = arr.length - 1;
+        
         while (i <= j) {
             int middle = i + (j - i) / 2; // To avoid array index out of bound we add i
             if (num == arr[middle]) {
-                System.out.println("Found in");
+                System.out.println("Found");
                 return;
             } else if (num > arr[middle]) {
                 i = middle + 1;
@@ -20,6 +22,7 @@ public class BinarySearch {
                 j = middle - 1;
             }
         }
+        
         System.out.println("Not Found");
 
     }
