a27f3304a624ad024c8e745aa5ca317fe316e45f
NIKIL S <107380156+Nikil2580@users.noreply.github.com>
2023-03-10 22:22:57 +0530

    Update SelectionSort.java

--- Sorting_Algorithms/SelectionSort.java
+++ Sorting_Algorithms/SelectionSort.java
@@ -7,9 +7,11 @@ public class SelectionSort {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int arr[] = new int[n];
+        
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+        
         for (int i = 0; i < n - 1; i++) {
             int Index = i;
             for (int j = i + 1; j < n; j++) {
@@ -21,6 +23,7 @@ public class SelectionSort {
             arr[Index] = arr[i];
             arr[i] = temp;
         }
+        
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
