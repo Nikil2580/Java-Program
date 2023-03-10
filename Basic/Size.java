cea60ee3cfb70c931cbca17fb810eb355324240c
Nikil2580 <cyanikil002@gmail.com>
2023-03-10 21:50:54 +0530

    Size of all primitive data types

---
+++ Basic/Size.java
@@ -0,0 +1,20 @@
+public class Size {
+    public static void main(String[] args) {
+        System.out.println("Size of byte:   " + Byte.BYTES + "\n");
+
+        System.out.println("Size of short:  " + Short.BYTES + "\n");
+
+        System.out.println("Size of int:    " + Integer.BYTES + "\n");
+
+        System.out.println("Size of long:   " + Long.BYTES + "\n");
+
+        System.out.println("Size of float:  " + Float.BYTES + "\n");
+
+        System.out.println("Size of double: " + Double.BYTES + "\n");
+
+        System.out.println("Size of char:   " + Character.BYTES);
+
+        // System.out.println("Size of Boolean:" + Boolean.BYTES);
+        // C.E : Size depends on JVM so Size of Byte is not Fixed
+    }
+}
