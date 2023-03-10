addf757dcbe831d0b7657fb7434a5f4a7fd370fd
Nikil2580 <cyanikil002@gmail.com>
2023-03-10 21:58:43 +0530

    Updated

--- Basic/StaticUsage.java
+++ Basic/StaticUsage.java
@@ -1,9 +1,14 @@
 class StaticUsage {
+
     static int count = 0;
 
     StaticUsage() { // Constructor
         count++;
         System.out.println(count);
+    }
+
+    static {
+        System.out.println("It is automatically called without any object");
     }
 
     public static void main(String args[]) {
