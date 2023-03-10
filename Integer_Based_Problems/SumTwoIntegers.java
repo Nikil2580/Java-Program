1fbf15d9dc8d69157b5ef1f3cf99161ae2447ea0
NIKIL S <107380156+Nikil2580@users.noreply.github.com>
2023-03-10 22:19:39 +0530

    Update SumTwoIntegers.java

--- Integer_Based_Problems/SumTwoIntegers.java
+++ Integer_Based_Problems/SumTwoIntegers.java
@@ -1,7 +1,7 @@
 public class SumTwoIntegers {
     public static void main(String[] args) {
         int a = 10;
-        int b = 10;
+        int b = 50;
         System.out.println("Sum = " + a + b);
     }
 }
