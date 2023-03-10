ed4ffbf758814acc788bb3fe57675d9c835bc755
Nikil2580 <cyanikil002@gmail.com>
2023-03-10 21:49:05 +0530

    Hello World in Tamil

---
+++ Basic/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("வணக்கம் உலகம்"); // Java supports Unicode so it supports many languages
+    } // ASCII is the subset of Unicode
+}
