89a2b3635600b73182090f0f6f7ff6d4d68eb326
Nikil2580 <cyanikil002@gmail.com>
2023-03-12 20:01:57 +0530

    Empty name for java file

---
+++ Basic/.java
@@ -0,0 +1,6 @@
+class A {
+    public static void main(String[] args) {
+        System.out.println("Empty file name"); // Java allows to save our java file by .java only, we need to compile it
+                                               // by javac .java and run by java classname
+    }
+}
